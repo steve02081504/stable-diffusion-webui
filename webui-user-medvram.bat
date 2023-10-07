05014b8e0a3f5a7e05402b29dbe59d6c36738e73
steve02081504 <steve02081504@EDEN.fukwold>
2023-10-07 23:18:01 +0800

    Update webui-user-medvram.bat

--- webui-user-medvram.bat
+++ webui-user-medvram.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --medvram
+set COMMANDLINE_ARGS= --medvram --skip-python-version-check --skip-torch-cuda-test
 set ACCELERATE=
 
 call webui.bat
